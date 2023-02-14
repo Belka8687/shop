8b6c2b49055dbd70b3df39f21f94e83ac12d5f71
Belka8687 <125367452+Belka8687@users.noreply.github.com>
2023-02-14 14:53:37 +0200

    finish

---
+++ build/precache-manifest.cd9ecbedf8e025c0f8d2e3456532bbb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913bdd89bdfb0b62ed3eaab0d860929a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71f259e052b59e705b42",
+    "url": "/static/css/main.812602da.chunk.css"
+  },
+  {
+    "revision": "ebfbe709bca6e8f6eff8",
+    "url": "/static/js/2.d01efb20.chunk.js"
+  },
+  {
+    "revision": "71f259e052b59e705b42",
+    "url": "/static/js/main.0fb4f8a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
